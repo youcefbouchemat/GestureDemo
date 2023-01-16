af08a5b3db2d625b9704433fd6b21631062dc441
apple <apple@MacBook-Pro.local>
2023-01-16 17:24:46 +0100

    initial commit

---
+++ GestureDemo/GestureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureDemoApp.swift
+//  GestureDemo
+//
+//  Created by apple on 16/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct GestureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
