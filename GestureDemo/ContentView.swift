f98de8401c019695740fbf1e32bb9a615e209bb0
apple <apple@MacBook-Pro.local>
2023-01-16 17:38:35 +0100

    add Long Press Gesture

--- GestureDemo/ContentView.swift
+++ GestureDemo/ContentView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tap = TapGesture()
+        .onEnded(){ _ in
+            print("Tapped now")
+        }
+    
+    let longPress = LongPressGesture()
+        .onEnded(){ _ in
+            print("Long press now")
+        }
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-                .gesture(
-                    TapGesture()
-                        .onEnded{ _ in
-                            print("Tapped")
-                        })
+                .gesture(tap)
+                .gesture(longPress)
             Text("Hello, world!")
         }
         .padding()
